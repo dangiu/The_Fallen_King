2a8c8e9c47bfa76dfa22357555c3f907915ac5a2
XNightHawk <x.nighth.x@gmail.com>
2015-01-01 19:34:39 +0100

    Riordino file repository

--- src/core/RangedEntity.java
+++
@@ -1,9 +0,0 @@
-package core;
-
-public interface RangedEntity extends Entity {
-	/**
-	 * 
-	 * @return Il numero di metri a cui l'entita' puo' iniziare la sua azione
-	 */
-	public double getRange();
-}
